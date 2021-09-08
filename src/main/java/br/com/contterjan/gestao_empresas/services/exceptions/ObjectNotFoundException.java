4a052722f23e80541f3542af59ba28e9c853a08a
Gabriel Oliveira <studying.online@outlook.com>
2021-09-08 08:17:32 -0300

    Serviço de inserção de todas as cidades e estados do Brasil no banco de dados

--- src/main/java/br/com/contterjan/gestao_empresas/services/exceptions/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.contterjan.gestao_empresas.services.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public ObjectNotFoundException(String message) {
-		super(message);
-	}
-}
